53d8ba2940060e92c50149aa9e202b2cac742281
Sittichai Thammawat <alonenew01@gmail.com>
2022-02-07 18:02:16 +0700

    deploy

--- config/config.js
+++ config/config.js
@@ -13,15 +13,15 @@ types.setTypeParser(1114, function(stringValue) {
 const DATABASE_URL = 'postgres://xcrqlvdjuewpqa:b47e0a9ded724881ad3f87f310973286bd892642aa61ff663dd79f42bbb49259@ec2-34-205-46-149.compute-1.amazonaws.com:5432/ddu5j2cj0mpq7m';
 
 const databaseConfig = {
-    'host': 'localhost',
+    'host': 'ec2-34-205-46-149.compute-1.amazonaws.com',
     'port': 5432,
-    'database': 'delivery_db',
-    'user': 'postgres',
-    'password': 'Plusomega01'
+    'database': 'ddu5j2cj0mpq7m',
+    'user': 'xcrqlvdjuewpqa',
+    'password': 'b47e0a9ded724881ad3f87f310973286bd892642aa61ff663dd79f42bbb49259'
 };
 
 
-const db = pgp(databaseConfig);
-// const db = pgp(DATABASE_URL);
+//const db = pgp(databaseConfig);
+const db = pgp(DATABASE_URL);
 
 module.exports = db;
