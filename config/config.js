07176900be48048c16daf8dc8c032091b5482977
Sittichai Thammawat <alonenew01@gmail.com>
2022-02-07 18:08:27 +0700

    deploy

--- config/config.js
+++ config/config.js
@@ -17,11 +17,14 @@ const databaseConfig = {
     'port': 5432,
     'database': 'ddu5j2cj0mpq7m',
     'user': 'xcrqlvdjuewpqa',
-    'password': 'b47e0a9ded724881ad3f87f310973286bd892642aa61ff663dd79f42bbb49259'
+    'password': 'b47e0a9ded724881ad3f87f310973286bd892642aa61ff663dd79f42bbb49259',
+    ssl: {
+        rejectUnauthorized: false
+    }
 };
 
 
-//const db = pgp(databaseConfig);
-const db = pgp(DATABASE_URL);
+const db = pgp(databaseConfig);
+//const db = pgp(DATABASE_URL);
 
 module.exports = db;
