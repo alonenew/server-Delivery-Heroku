67b5e61f3bc97c60031ea9a919ab7120425dbc51
Sittichai Thammawat <alonenew01@gmail.com>
2022-02-08 18:32:48 +0700

    add heroku

--- config/config.js
+++ config/config.js
@@ -24,11 +24,4 @@ const databaseConfig = {
 };
 const db = pgp(databaseConfig);
 
-
-
-//const DATABASE_URL = 'postgres://xcrqlvdjuewpqa:b47e0a9ded724881ad3f87f310973286bd892642aa61ff663dd79f42bbb49259@ec2-34-205-46-149.compute-1.amazonaws.com:5432/ddu5j2cj0mpq7m';
-//const db = pgp(DATABASE_URL);
-
-
-
 module.exports = db;
